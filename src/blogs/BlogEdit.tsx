d8f1a310c0e16be3f7d6232687c06f6f9f1519c4
SouliyaPPS-7575 <souliyapps@gmail.com>
2025-07-08 22:06:07 +0700

    🐞 fix(upload image in blog):

--- src/blogs/BlogEdit.tsx
+++ src/blogs/BlogEdit.tsx
@@ -43,7 +43,82 @@ export const BlogEdit = () => {
       <SimpleForm>
         {/* <TextField source='id' /> */}
         <TextInput source='title' validate={[required()]} fullWidth />
+      <div style={{ alignItems: 'center', gap: 8 }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 8,
+          }}
+        >
+          <button
+            type='button'
+            onClick={async (e) => {
+              e.preventDefault();
+              const input = document.createElement('input');
+              input.type = 'file';
+              input.accept = 'image/*';
+              input.onchange = async () => {
+                if (input.files && input.files[0]) {
+                  try {
+                    const { uploadImageToCloudinary } = await import(
+                      '../utils/cloudinaryUpload'
+                    );
+                    const url = await uploadImageToCloudinary(input.files[0]);
+                    // Copy image URL to clipboard
+                    await navigator.clipboard.writeText(url);
+                    // Show alert notification
+                    alert('Image link copied to clipboard!');
+                    // Find the contenteditable div of RichTextInput
+                    const rte = document.querySelector(
+                      '[contenteditable="true"][role="textbox"]'
+                    );
+                    if (rte) {
+                      // Insert image at the caret position
+                      const img = document.createElement('img');
+                      img.src = url;
+                      img.alt = 'uploaded image';
+                      // Insert image at caret
+                      const sel = window.getSelection();
+                      if (sel && sel.rangeCount > 0) {
+                        const range = sel.getRangeAt(0);
+                        range.collapse(false);
+                        range.insertNode(img);
+                        // Move caret after the image
+                        range.setStartAfter(img);
+                        range.setEndAfter(img);
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                      } else {
+                        rte.appendChild(img);
+                      }
+                      // Trigger input event to update form value
+                      rte.dispatchEvent(new Event('input', { bubbles: true }));
+                    }
+                  } catch (err) {
+                    alert('Failed to upload image');
+                  }
+                }
+              };
+              input.click();
+            }}
+            style={{
+              marginRight: 8,
+              cursor: 'pointer',
+              padding: '8px 16px',
+              backgroundColor: '#3f51b5',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '4px',
+            }}
+          >
+            Upload Image
+          </button>
+        </div>
         <RichTextInput source='description' validate={[required()]} fullWidth />
+      </div>
+
         <ImageInput
           source='image'
           label='Blog Image'
