9338a3fe09eee022727e72009f60489db1e64910
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-07-09 16:03:23 +0000

    deploy: e2c7e55defa7430ada0b230f5256c32fcd38397a

--- assets/rest-D4Phqynt.js
+++
@@ -1,2 +0,0 @@
-import{s as t}from"./index-CzLHtuwF.js";const o=t("http://localhost:4000");export{o as default};
-//# sourceMappingURL=rest-D4Phqynt.js.map
---
+++ assets/rest-PYR3FTXR.js
@@ -0,0 +1,2 @@
+import{s as t}from"./index-BkLV5lzf.js";const o=t("http://localhost:4000");export{o as default};
+//# sourceMappingURL=rest-PYR3FTXR.js.map
