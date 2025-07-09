6ca037abb6cc5a2192e5e97687c02220e3b2852b
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-07-09 14:34:41 +0000

    deploy: 24e901d6d8bb11057b316e00482c7c8d9710e896

--- assets/rest-CjGeHyTq.js
+++
@@ -1,2 +0,0 @@
-import{aD as t}from"./vendor-B_ZA2uik.js";import"./vendor_react-GZKCY3cZ.js";import"./vendor_mui-Cnfv-Sgz.js";const e=t("http://localhost:4000");export{e as default};
-//# sourceMappingURL=rest-CjGeHyTq.js.map
---
+++ assets/rest-D4Y_ACcK.js
@@ -0,0 +1,2 @@
+import{aD as t}from"./vendor-ue79-INe.js";import"./vendor_react-CbcXT_7b.js";import"./vendor_mui-Hs1nlRIC.js";const e=t("http://localhost:4000");export{e as default};
+//# sourceMappingURL=rest-D4Y_ACcK.js.map
