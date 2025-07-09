c8eee350ba3017f7d526fed202be62987961cbdd
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-07-09 15:27:05 +0000

    deploy: 66acb90c78a31261da10eb36a87ba8866e9c5f5a

---
+++ assets/rest-DysBhEkA.js
@@ -0,0 +1,2 @@
+import{aD as t}from"./vendor-BTHUKa31.js";import"./vendor_react-W3U44I7N.js";import"./vendor_mui-CYFAI7s6.js";const e=t("http://localhost:4000");export{e as default};
+//# sourceMappingURL=rest-DysBhEkA.js.map
