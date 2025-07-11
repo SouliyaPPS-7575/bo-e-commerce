55fdfab41cb49cba00da87345c4cf0198cf7f118
SouliyaPPS-7575 <SouliyaPPS-7575@users.noreply.github.com>
2025-07-11 09:45:52 +0000

    deploy: ffa0c264cd620254fe0dc104b63ef03db51175e6

--- assets/rest-Bd5nMYsj.js
+++
@@ -1,2 +0,0 @@
-import{s as t}from"./index-CG3ptp7W.js";const o=t("http://localhost:4000");export{o as default};
-//# sourceMappingURL=rest-Bd5nMYsj.js.map
---
+++ assets/rest-CIOr_ch5.js
@@ -0,0 +1,2 @@
+import{s as t}from"./index-DdCcaZ0n.js";const o=t("http://localhost:4000");export{o as default};
+//# sourceMappingURL=rest-CIOr_ch5.js.map
